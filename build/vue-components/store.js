8d54cedb6f24970103534503e57e3116fd002ee6
Garion Herman <garion@silverstripe.com>
2016-07-30 16:16:35 +1200

    Adds RegisterView, updates Nav, ignores compiled JS / CSS.

--- build/vue-components/store.js
+++ build/vue-components/store.js
@@ -4,13 +4,25 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-	thing: 'default value'
+	thing: 'lol',
+	registered: false,
+	user: {
+		name: '',
+		email: '',
+		password: ''
+	}
 }
 
 const mutations = {
     // Data Import Mutations
     CHANGE_SOMETHING(state, newvalue) {
         state.thing = newvalue
+    },
+    SET_USER(state, user) {
+    	state.user = user
+    },
+    SET_REGISTERED(state, value) {
+    	state.registered = value
     }
 }
 
